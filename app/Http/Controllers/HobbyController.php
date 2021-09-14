312231802699ff9a79700a7767e27173a8d34ef9
pszewczykpl <pszewczykpl@gmail.com>
2021-09-14 20:00:06 +0200

    Added files to feature: hobbies

---
+++ app/Http/Controllers/HobbyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hobby;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HobbyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Hobby  $hobby
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hobby $hobby)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Hobby  $hobby
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hobby $hobby)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Hobby  $hobby
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hobby $hobby)
+    {
+        //
+    }
+}
